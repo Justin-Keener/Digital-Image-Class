39d23d8bcdd9ec0270a69936a6fa101d400151a8
Justin Keener <king.keenz@gmail.com>
2018-01-22 14:42:46 -0500

    Updated working python notebook

--- HW/HW 1.py
+++ HW/HW 1.py
@@ -1,23 +1,25 @@
-# Names
 import os
 import matplotlib.pyplot as plt
 import cv2
 
 # Declares the images
-im_1 = cv2.imread("/Users/justinkeener/Desktop/Digital Image Pattern/HW/HW 1 /ngc6357_4k.jpg")
-im_2 = cv2.imread("/Users/justinkeener/Desktop/Digital Image Pattern/HW/HW 1 /ngc6357_4k.png")
+im1_path = "/Users/justinkeener/Desktop/Digital-Image-Class/images/ngc6357_4k.jpg"
+im2_path = "/Users/justinkeener/Desktop/Digital-Image-Class/images/ngc6357_4k.png"
+
+im1 = cv2.imread(im1_path)
+im2 = cv2.imread(im2_path)
 
 # Displays two subplot images into one figure
 plt.figure(1)
 plt.subplot(121)
 plt.title('Chandra Galaxy.jpg')
-plt.imshow(cv2.cvtColor(im_1, cv2.COLOR_BGR2RGB))
+plt.imshow(cv2.cvtColor(im1, cv2.COLOR_BGR2RGB))
 
 plt.subplot(122)
 plt.title('Chandra Galaxy.png')
-plt.imshow(cv2.cvtColor(im_2, cv2.COLOR_BGR2RGB))
+plt.imshow(cv2.cvtColor(im2, cv2.COLOR_BGR2RGB))
 plt.show()
 
 # Prints the infomartion to compare the file sizes
-print('Size of the jpeg file in bytes: ', os.stat("/Users/justinkeener/Desktop/Digital Image Pattern/HW/HW 1 /ngc6357_4k.jpg").st_size)
-print('Size of the png file in bytes: ', os.stat("/Users/justinkeener/Desktop/Digital Image Pattern/HW/HW 1 /ngc6357_4k.png").st_size)
+print('Size of the jpeg file in bytes: ', os.stat(img1_path).st_size)
+print('Size of the png file in bytes: ', os.stat(im2_path).st_size)
